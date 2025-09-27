ba7ee4048e00c7c1de350a9b6f7451ca3610d53f
Kim Jaehun <hun970412@naver.com>
2025-09-27 22:53:24 +0900

    commnet: [week01] 소수찾기 - 주석 추가

--- algorithm-study/src/main/java/week01/boj1978/소수찾기_김재훈.java
+++ algorithm-study/src/main/java/week01/boj1978/소수찾기_김재훈.java
@@ -12,7 +12,7 @@ public class 소수찾기_김재훈 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // N 활용 x
         String[] split = br.readLine().split(" ");
         int count = 0;
 
@@ -20,10 +20,12 @@ public class 소수찾기_김재훈 {
             int num = Integer.parseInt(s);
             boolean isPrime = true;
 
+            // 0과 1은 소수가 아니므로 통과
             if (num == 0 || num == 1) {
                 continue;
             }
 
+            // 2부터 (자기 숫자 -1)까지 나눠봤을 때 나눠지는 수가 없어야 소수
             for (int i = 2; i < num; i++) {
                 if (num % i == 0) {
                     isPrime = false;
