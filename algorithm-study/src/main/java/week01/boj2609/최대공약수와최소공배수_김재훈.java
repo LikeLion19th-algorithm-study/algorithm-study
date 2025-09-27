4041237e8e8fbe4beb6a552adad9c776ff29d150
Kim Jaehun <hun970412@naver.com>
2025-09-27 22:56:13 +0900

    comment: [week01] 최대공약수와 최소공배수 - 주석 추가

--- algorithm-study/src/main/java/week01/boj2609/최대공약수와최소공배수_김재훈.java
+++ algorithm-study/src/main/java/week01/boj2609/최대공약수와최소공배수_김재훈.java
@@ -17,6 +17,7 @@ public class 최대공약수와최소공배수_김재훈 {
         int a = Integer.parseInt(split[0]);
         int b = Integer.parseInt(split[1]);
 
+        // b가 더 큰 수일 경우 순서 교체
         if (b > a) {
             int temp = a;
             a = b;
@@ -30,14 +31,18 @@ public class 최대공약수와최소공배수_김재훈 {
         System.out.println(lcn);
     }
 
+    // 최대 공약수
     static int gcd(int a, int b) {
+        // 나누었을 때 0이 되는 값이 최대 공약수
         if (a % b == 0) {
             return b;
         }
 
+        // 재귀 호출
         return gcd(b, a % b);
     }
 
+    // 최소 공배수
     static int lcn(int a, int b) {
         return a * b / gcd(a, b);
     }
