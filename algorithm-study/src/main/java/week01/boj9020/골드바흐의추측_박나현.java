9e5f29385bab118248f8f0eedb859d59105303f1
nahyuunn <138644012+nahyuunn@users.noreply.github.com>
2025-09-29 08:43:48 +0900

    add: [week01] 골드바흐의추측 - 코드 추가

---
+++ algorithm-study/src/main/java/week01/boj9020/골드바흐의추측_박나현.java
@@ -0,0 +1,69 @@
+package week01.boj9020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 골드바흐의추측_박나현 {
+
+    static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        int[] nums = new int[count];
+        int max = 0;
+
+        for (int i = 0; i < count; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, nums[i]);
+        }
+
+        sieve(max);
+
+        for (int i = 0; i < count; i++) {
+            int n  = nums[i];
+            int p = getSmallPrime(n);
+            sb.append(p).append(" ").append(n - p).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void sieve(int n) {
+        isPrime = new boolean[n+1];
+
+        for (int i = 0; i < isPrime.length; i++) {
+            isPrime[i] = true;
+        }
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for(int j = i*i; j<= n; j += i){//그 이하의 수는 모두 검사했으므로 i*i부터 시작
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+//    private static boolean isPrime(int n) {
+//        if (n < 2) return false;
+//        else if (n == 2) return true;
+//        else {
+//            for (int j = 2; j * j <= n; j++) {
+//                if (n % j == 0) return false;
+//            }
+//        }
+//        return true;
+//    }
+    private static int getSmallPrime(int n) {
+        for (int i = n/2; i >= 2; i--) {
+            if (isPrime[i] && isPrime[n-i]){
+                return i;
+            }
+        }
+        return 2;
+    }
+}
