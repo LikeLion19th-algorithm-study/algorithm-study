508918eecf1f97a369af3046b65b39c400f9c57a
Kim Jaehun <hun970412@naver.com>
2025-09-30 02:02:37 +0900

    refactor: [week01] 골드바흐의 추축 - StringBuilder 사용 및 주석 추가

--- algorithm-study/src/main/java/week01/boj9020/골드바흐의추측_김재훈.java
+++ algorithm-study/src/main/java/week01/boj9020/골드바흐의추측_김재훈.java
@@ -8,6 +8,8 @@ import java.util.Arrays;
 /**
  * 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다.
  * 여러 가지인 경우에는 두 소수의 차이가 가장 작은 것을 출력한다.
+ * StringBuilder 사용 전 수행시간: 376ms
+ * StringBuilder 사용 후 수행시간: 276ms
  */
 public class 골드바흐의추측_김재훈 {
 
@@ -15,6 +17,7 @@ public class 골드바흐의추측_김재훈 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         // 테스트 케이스의 개수
         int k = Integer.parseInt(br.readLine());
@@ -27,15 +30,17 @@ public class 골드바흐의추측_김재훈 {
 
             boolean[] prime = findPrime(n);
 
+            // 중간에서 한 칸씩 이동하며 찾으면 가장 가까운 수를 찾을 수 있다.
             while (true) {
                 if (prime[i1] && prime[i2]) {
-                    System.out.println(i1 + " " + i2);
+                    sb.append(i1).append(" ").append(i2).append("\n");
                     break;
                 }
                 i1--;
                 i2++;
             }
         }
+        System.out.println(sb);
     }
 
     private static boolean[] findPrime(int n) {
@@ -43,6 +48,11 @@ public class 골드바흐의추측_김재훈 {
         Arrays.fill(isPrime, true);
         isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아니다.
 
+        /*
+          2가 소수라면, 4, 6, 8, 10, ...
+          3이 소수라면, 9, 12, 15, 18, ...
+          이런 식으로 소수의 배수가 되는 수는 모두 제거한다.
+         */
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
