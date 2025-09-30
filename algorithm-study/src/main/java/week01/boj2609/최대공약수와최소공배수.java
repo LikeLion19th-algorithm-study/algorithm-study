dac2ea499f30ecba50cc40271438ab9969d00385
haein <93hilee@naver.com>
2025-09-30 14:19:26 +0900

    add: [week01] 최대공약수와 최소공배수 - 코드 추가

---
+++ algorithm-study/src/main/java/week01/boj2609/최대공약수와최소공배수.java
@@ -0,0 +1,36 @@
+package week01.boj2609;
+
+// 문제: 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+// 입력: 첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+// 출력: 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+// 예제입력: 24 18
+// 예제출력:
+// 6
+// 72
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 최대공약수와최소공배수 {
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long g = gcd(a, b);
+        long l = (a / g) * b;
+
+        System.out.println(g);
+        System.out.println(l);
+    }
+}
