8cfd8fca8c2cb59d5ba468837a9341a30dc1cb91
haein <93hilee@naver.com>
2025-10-14 19:50:02 +0900

    add: [week02] 코드 추가
    작성 완료 문제
    - boj1764
    - boj17219
    - pgs42577

---
+++ algorithm-study/src/main/java/week02/boj17219/비밀번호찾기.java
@@ -0,0 +1,32 @@
+package week02.boj17219;
+
+import java.io.*;
+import java.util.*;
+
+public class 비밀번호찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String site = st.nextToken();
+            String password = st.nextToken();
+            map.put(site, password);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            String site = br.readLine();
+            sb.append(map.get(site)).append('\n'); // 비밀번호 조회 후 저장
+        }
+
+        System.out.print(sb);
+    }
+}
---
+++ algorithm-study/src/main/java/week02/pgs42576/problem.md
@@ -0,0 +1,26 @@
+# [완주하지 못한 선수](https://school.programmers.co.kr/learn/courses/30/lessons/42576)
+
+## 문제
+
+
+---
+
+## 입력
+
+
+---
+
+## 출력
+
+
+---
+
+## 예제 입력
+```markdown
+
+```
+
+## 예제 출력
+```markdown
+
+```
