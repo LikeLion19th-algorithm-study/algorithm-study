ecee96f10578b2ffe0050a6b04f95c155eb80670
Kim Jaehun <hun970412@naver.com>
2025-10-20 03:35:48 +0900

    comment: [week03] 스택 - 주석 추가

--- algorithm-study/src/main/java/week03/boj10773/스택_김재훈.java
+++ algorithm-study/src/main/java/week03/boj10773/스택_김재훈.java
@@ -1,8 +1,6 @@
 package week03.boj10773;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -27,44 +25,41 @@ public class 스택_김재훈 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
 
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); // 명령의 수
 
         while (n-- > 0) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine()); // 명령 읽기
             String command = st.nextToken();
 
             switch (command) {
-                case "push":
-                    int num = Integer.parseInt(st.nextToken());
+                case "push": // 정수 X를 스택에 넣는 연산.
+                    int num = Integer.parseInt(st.nextToken()); // 정수 X
                     stack.push(num);
                     break;
-                case "pop":
+                case "pop": // 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력. 만약 스택에 들어있는 정수가 없는 경우 -1을 출력.
                     if (stack.isEmpty()) {
                         sb.append(-1).append("\n");
-//                        System.out.println(-1);
                         break;
                     }
-//                    System.out.println(stack.pop());
                     sb.append(stack.pop()).append("\n");
                     break;
-                case "size":
-//                    System.out.println(stack.size());
+                case "size": // 스택에 들어있는 정수의 개수를 출력.
                     sb.append(stack.size()).append("\n");
                     break;
-                case "empty":
-//                    System.out.println(stack.isEmpty() ? 1 : 0);
+                case "empty": // 스택이 비어있으면 1, 아니면 0을 출력.
                     sb.append(stack.isEmpty() ? 1 : 0).append("\n");
                     break;
-                case "top":
-//                    System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                case "top": // 스택의 가장 위에 있는 정수를 출력. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력.
                     sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
                     break;
             }
         }
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
