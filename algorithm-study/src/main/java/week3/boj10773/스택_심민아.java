b80653f82557b2a86f96810ba88908b249328271
simmina <s1mm1n6h@gmail.com>
2025-10-21 17:18:13 +0900

    add: [week03] 스택 - 코드 추가

---
+++ algorithm-study/src/main/java/week3/boj10773/스택_심민아.java
@@ -0,0 +1,48 @@
+package week3.boj10773;
+
+import java.util.*;
+import java.io.*;
+
+public class 스택_심민아 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] stack = new int[N];
+        int top = -1;
+
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push": {
+                    int x = Integer.parseInt(st.nextToken());
+                    stack[++top] = x;
+                    break;
+                }
+                case "pop": {
+                    if (top == -1) sb.append(-1).append('\n');
+                    else sb.append(stack[top--]).append('\n');
+                    break;
+                }
+                case "size": {
+                    sb.append(top + 1).append('\n');
+                    break;
+                }
+                case "empty": {
+                    sb.append(top == -1 ? 1 : 0).append('\n');
+                    break;
+                }
+                case "top": {
+                    if (top == -1) sb.append(-1).append('\n');
+                    else sb.append(stack[top]).append('\n');
+                    break;
+                }
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
