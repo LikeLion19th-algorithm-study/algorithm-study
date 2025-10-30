f427be71c4ca7c05bbf1a8884fe2ef5f4e93bb90
nahyuunn <138644012+nahyuunn@users.noreply.github.com>
2025-10-30 15:05:37 +0900

    rename: [weeo04] 카드2 - 파일명 변경

--- algorithm-study/src/main/java/week04/boj2164/카드2_박나현.java
+++ algorithm-study/src/main/java/week04/boj2164/카드2_박나현.java
@@ -3,7 +3,7 @@ package week04.boj2164;
 import java.io.*;
 import java.util.*;
 
-public class 카드_박나현 {
+public class 카드2_박나현 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -19,7 +19,6 @@ public class 카드_박나현 {
             q.poll();
             q.offer(q.poll());
         }
-
         System.out.println(q.poll());
     }
 }
