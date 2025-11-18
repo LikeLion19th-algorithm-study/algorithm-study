83930712518d2ff7f7733f9ed6dcc7b8709eabf0
Kim Jaehun <hun970412@naver.com>
2025-11-18 19:52:29 +0900

    refactor: [week06] 절대값 힙 - 조건문 중복 제거 후 삼항연산자 사용

--- algorithm-study/src/main/java/week06/boj11286/절대값힙_김재훈.java
+++ algorithm-study/src/main/java/week06/boj11286/절대값힙_김재훈.java
@@ -11,7 +11,7 @@ import java.util.PriorityQueue;
  * x가 0이 아니라면 배열에 x 추가, x가 0이라면 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거
  * 절댓값이 가장 작은 값이 여러 개일 때는 가장 작은 수를 출력하고 그 값을 배열에서 제거
  * 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우 0을 출력
- * 메모리: 26596KB | 시간: 276ms
+ * 메모리: 24752KB | 시간: 280ms
  */
 public class 절대값힙_김재훈 {
 
@@ -32,14 +32,11 @@ public class 절대값힙_김재훈 {
         while (n-- > 0) {
             int x = Integer.parseInt(br.readLine()); // 연산에 대한 정보를 나타내는 정수 x
 
-            if (x == 0) {
-                if (pq.isEmpty()) { // 배열이 비어 있는 경우인데 출력하라고 한 경우
-                    sb.append(0).append("\n"); // 0을 출력한다.
-                    continue;
-                }
-                sb.append(pq.poll()).append("\n");
-            } else {
-                pq.offer(x);
+            if (x != 0) { // x가 0이 아니라면
+                pq.offer(x); // 값 추가
+            } else { // x가 0이라면
+                // 배열이 비어있다면 0, 비어있지 않다면 값 출력
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
             }
         }
         System.out.println(sb);
