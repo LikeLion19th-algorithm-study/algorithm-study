83c39c2007a4b89e3eded0b4397db3aa07bf082d
haein <93hilee@naver.com>
2025-12-02 21:12:23 +0900

    add: [week07] 피보나치수5 - 코드 추가

---
+++ algorithm-study/src/main/java/week07/bOJ10870/피보나치수5_이해인.java
@@ -0,0 +1,18 @@
+package week07.bOJ10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 피보나치수5_이해인 {
+    public static int fib(int n) {
+        if (n < 2) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fib(n));
+    }
+}
